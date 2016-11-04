6411f6754d8e8ebe4491038b3913e01dce27df57
Fuminides <javierfumanalidocin@gmail.com>
2016-11-04 17:59:31 +0100

    Terminado el parseo de la consulta

--- RI/parser/Parser.java
+++ RI/parser/Parser.java
@@ -3,8 +3,17 @@ package parser;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.lucene.analysis.es.SpanishAnalyzer;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.util.Version;
+
+import trabajo.SearchFiles;
+
 public class Parser {
 
+	int anyoActual = 2016;
 	//Hardcodeado por simpleza.
 	private String[] diccionarioLenguajes = 
 		{ "frances", "español", "ingles", "portugues",
@@ -95,14 +104,89 @@ public class Parser {
 	}
 	
 	public String buscarFecha(){
-		String resultado = "";
-		//TODO
+		String resultado = "",
+				pattern = "entre ([0-9])+ y ([0-9])+",
+				pattern2 = "año ([0-9])+",
+				pattern3 = "ultimos ([0-9])+ años",
+				patternNumber = "([0-9])+";
+		
+		Pattern p = Pattern.compile(pattern);
+		 Matcher m = p.matcher(frase);
+		 boolean b = m.find();
+		 if ( b ){
+			 p = Pattern.compile(patternNumber);
+			 m = p.matcher(m.group(0));
+			 m.find();
+			 String anyo1 = m.group(0);
+			 m.find();
+			 String anyo2 = m.group(0); 
+			 frase = frase.replace("anyo1", "");
+			 frase = frase.replace("anyo2", "");
+			 return anyo1 + "," + anyo2;
+		 }
+		 
+		 p = Pattern.compile(pattern2);
+		 m = p.matcher(frase);
+		 b = m.find();
+		 
+		 if ( b ){
+			 p = Pattern.compile(patternNumber);
+			 m = p.matcher(frase);
+			 m.find();
+			 frase = frase.replace("año " + m.group(0), "");
+			 return m.group(0);
+		 }
+		 
+		 p = Pattern.compile(pattern3);
+		 m = p.matcher(frase);
+		 b = m.find();
+		 
+		 if ( b ){
+			 p = Pattern.compile(patternNumber);
+			 m = p.matcher(frase);
+			 m.find();
+			 frase = frase.replace("ultimos " + m.group(0) + " años", "");
+			 return (anyoActual - Integer.parseInt(m.group(0))) + "," + anyoActual;
+		 }
+		
 		return resultado;
 	}
 	
-	public static void main(String[] args){
-		Parser parser = new Parser("Me interesa encontrar textos sobre información geológica de España.");
-		System.out.println(parser.buscarAutor());
+	public Query execute() throws ParseException{
+		String id = buscarID(), publisher = buscarPublisher(),
+				buscarAutor = buscarAutor(), buscarFecha = buscarFecha(), fecha = SearchFiles.FECHA +":", 
+				tema = SearchFiles.TEMATICO + ":", autor = SearchFiles.AUTOR + ":", resultado = "";
+	    QueryParser parser = new QueryParser(Version.LUCENE_44, SearchFiles.TEMATICO, new SpanishAnalyzer(Version.LUCENE_44));
+
+		if ( id != "" ){
+			autor += id;
+			 frase = frase.replaceAll(id, "");
+		}
+		if ( publisher != ""){
+			if (autor.charAt(autor.length()-1) == ':' ) autor += publisher;
+			else autor += "," + publisher;
+			 frase = frase.replaceAll(autor, "");
+		}
+		if ( buscarAutor != ""){
+			if (autor.charAt(autor.length()-1) == ':' ) autor += buscarAutor;
+			else autor += "," + buscarAutor;
+			 frase = frase.replaceAll(autor, "");
+		}
+		
+		if ( buscarFecha != ""){
+			fecha += buscarFecha;
+		}
+		
+		resultado += tema + frase;
+		if ( fecha != SearchFiles.FECHA+":" ) resultado += " " + fecha;
+		if ( autor != SearchFiles.AUTOR +":" ) resultado += " " + autor;
+	    
+		return parser.parse(resultado);
+	}
+	
+	public static void main(String[] args) throws ParseException{
+		Parser pars = new Parser("Un usuario necesita obtener técnicas de inteligencia artificial para implementarlas en un videojuego. Ofrecer proyectos relacionados con la inteligencia artificial en los ultimos 5 años.");
+		System.out.println(pars.execute());
 	}
 
 }
